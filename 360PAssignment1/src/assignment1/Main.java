bc0e14e1451cd713e75e548ad0d28bebdcf002fc
juliana leon <jewelsleon@gmail.com>
2015-02-02 18:41:32 -0600

    updated code

--- 360PAssignment1/src/assignment1/Main.java
+++ 360PAssignment1/src/assignment1/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	     // Test Q1 implementation
 	     // you do not need to write this code. 
 	     // create and populate array A with values
-	     //int A[] = {42,2,9,25,49}; 
-			int A[] = {10,11,42,2,9,25,49,88,100,33,66,4,22};
+	     int A[] = {42,2,9,25,49}; 
+		//int A[] = {10,11,42,2,9,25,49,88,100,33,66,4,22};
 	     // call PSort 
 	     PSort.parallelSort(A, 0, A.length);
 	     
@@ -18,8 +18,8 @@ public class Main {
 	     // (you do not need to write this code). 
 
 	     // Test Q2 implementation with 3 threads
-	     int lookup = 66;
-	     int result = PSearch.parallelSearch(lookup, A, 4);
+	     int lookup = 22;
+	     int result = PSearch.parallelSearch(lookup, A, 14);
 	     System.out.println("\n" +"index is of " +lookup+ " is: " + result);
 	     // should return -1 as 20 is not in array
 	     // ... verification code --- written by us. 
--- 360PAssignment1/src/assignment1/PSort.java
+++ 360PAssignment1/src/assignment1/PSort.java
@@ -1,16 +1,47 @@
 package assignment1;
 
-public class PSort {
-	public static void parallelSort(int[] A, int begin, int end){
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PSort implements Runnable {
+	private int[] array;
+	private int start;
+	private int end;
 	
+	public PSort(int[] array, int start, int end){
+		this.array= array;
+		this.start= start;
+		this.end = end;
+	} 
+	
+	@Override
+    public void run() {
+		int pivot = array[start + ((end-start)/2)];
+		PSort psort1 = new PSort(array, start, pivot);
+		PSort psort2 = new PSort(array, pivot+1, end);
+
+		ExecutorService pool = Executors.newCachedThreadPool();
+		Future<?> runnableFuture1 = pool.submit(psort1);
+		Future<?> runnableFuture2 = pool.submit(psort2);
+		quickSort(array, start, end);
+    }
+	
+	public static void parallelSort(int[] A, int begin, int end){
 		if (A == null || end == 0){
 			return;
 		}
-		PSort psort = new PSort();
-		psort.quickSort(A, begin, end-1);
+		
+		//create cached thread pool
+		ExecutorService cachedPool = Executors.newCachedThreadPool();
+		PSort psort = new PSort(A, begin, end-1);
+		Future<?> runnableFuture = cachedPool.submit(psort);
+		//psort.quickSort(A, begin, end-1);
+		cachedPool.shutdown(); // shutdown the pool
 	}
 		
-	private void quickSort(int[] Array, int lower, int higher){
+	
+	public void quickSort(int[] Array, int lower, int higher){
 		int low = lower;
 		int high = higher;
 		int pivot = Array[low + ((high-low)/2)];
@@ -22,7 +53,7 @@ public class PSort {
 			while (Array[high] > pivot){
 				high --;
 			}
-			if(low <= high){
+			if(low <= high){				//swap
 				int temp = Array[low];
 				Array[low] = Array[high];
 				Array[high] = temp;
@@ -31,11 +62,13 @@ public class PSort {
 			}
 			
 			if(lower < high){
+				//runnableFuture.add(cachedPool.submit(new PSort(Array, lower, high)));
 				quickSort(Array, lower, high);
 			}
 			if(low < higher){
 				quickSort(Array, low, higher);
 			}
+			
 		}
 	}
 	
