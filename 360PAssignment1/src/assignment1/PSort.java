b930d036d3c0ba895720bbdb6490b05ed1b12e40
juliana leon <jewelsleon@gmail.com>
2015-02-03 01:52:21 -0600

    psort..

--- 360PAssignment1/src/assignment1/PSort.java
+++ 360PAssignment1/src/assignment1/PSort.java
@@ -6,18 +6,18 @@ import java.util.concurrent.Executors;
 
 public class PSort implements Runnable {
 	public static final int MAX_THREADS = Runtime.getRuntime().availableProcessors();
-    static final ExecutorService executor = Executors.newFixedThreadPool(5000);
+    static final ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
 	
 	private int[] array;
 	private int start;
 	private int end;
-	private final int minPartion;
+	private final int minPartition;
 	
-	public PSort(int minPartion, int[] array, int start, int end){
+	public PSort(int minPartition, int[] array, int start, int end){
 		this.array= array;
 		this.start= start;
 		this.end = end;
-		this.minPartion = minPartion;
+		this.minPartition = minPartition;
 	} 
 	
 	@Override
@@ -28,10 +28,10 @@ public class PSort implements Runnable {
 	public static void parallelSort(int[] A, int begin, int end){
 		
 		//create cached thread pool
-		//ExecutorService cachedPool = Executors.newCachedThreadPool();
+		//ExecutorService executor = Executors.newCachedThreadPool();
 		PSort psort = new PSort(A.length/PSort.MAX_THREADS, A, begin, end-1);
 		psort.run();
-		//Future<?> runnableFuture = cachedPool.submit(psort);
+		//Future<?> runnableFuture = executor.submit(psort);
 		//psort.quickSort(A, begin, end-1);
 		//cachedPool.shutdown(); // shutdown the pool
 	}
@@ -64,11 +64,11 @@ public class PSort implements Runnable {
 
         swap(array, storeIndex, end);
 
-        if (length > minPartion) {
+        if (length > minPartition) {
 
-        	PSort quick = new PSort(minPartion, array, start, storeIndex - 1);
-            Future<?> future = executor.submit(quick);
-           // PSort quick2 = new PSort(minPartion, array, storeIndex + 1, end);
+        	PSort t = new PSort(minPartition, array, start, storeIndex - 1);
+            Future<?> future = executor.submit(t);
+           // PSort quick2 = new PSort(minPartition, array, storeIndex + 1, end);
             quickSort(array, storeIndex + 1, end);
             //future = executor.submit(quick2);
             try {
