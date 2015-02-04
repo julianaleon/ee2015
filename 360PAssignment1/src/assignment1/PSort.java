d0048e82d69b289cf2b7e0cb56e0f2c8290e53b8
juliana leon <jewelsleon@gmail.com>
2015-02-03 18:01:26 -0600

    psort with 2 threads at a time

--- 360PAssignment1/src/assignment1/PSort.java
+++ 360PAssignment1/src/assignment1/PSort.java
@@ -1,56 +1,33 @@
 package assignment1;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class PSort implements Runnable {
-	public static final int MAX_THREADS = Runtime.getRuntime().availableProcessors();
-    static final ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
+	//create thread pool
+	ExecutorService executor = Executors.newCachedThreadPool();
 	
 	private int[] array;
 	private int start;
 	private int end;
-	private final int minPartition;
 	
-	public PSort(int minPartition, int[] array, int start, int end){
+	public PSort(int[] array, int start, int end){
 		this.array= array;
 		this.start= start;
 		this.end = end;
-		this.minPartition = minPartition;
 	} 
 	
 	@Override
     public void run() {
-		quickSort(array, start, end);	
-    }
-	
-	public static void parallelSort(int[] A, int begin, int end){
 		
-		//create cached thread pool
-		//ExecutorService executor = Executors.newCachedThreadPool();
-		PSort psort = new PSort(A.length/PSort.MAX_THREADS, A, begin, end-1);
-		psort.run();
-		//Future<?> runnableFuture = executor.submit(psort);
-		//psort.quickSort(A, begin, end-1);
-		//cachedPool.shutdown(); // shutdown the pool
-	}
-		
-	public void swap(int[] Array, int x, int y){
-		int temp = Array[x];
-		Array[x] = Array[y];
-		Array[y] = temp;
-	}
-	
-	
-	public void quickSort(int[] Array, int start, int end){
 		int length = end - start +1;
 		
 		if(length <= 1){return;}
 		
-		//int pivot_index = medianOfThree(array, start, end);
 		int pivot_index = start+(end-start)/2;
-		int pivotValue= Array[pivot_index];
+		int pivotValue= array[pivot_index];
 		
 		swap(array, pivot_index, end);
 
@@ -63,25 +40,47 @@ public class PSort implements Runnable {
         }
 
         swap(array, storeIndex, end);
-
-        if (length > minPartition) {
-
-        	PSort t = new PSort(minPartition, array, start, storeIndex - 1);
-            Future<?> future = executor.submit(t);
-           // PSort quick2 = new PSort(minPartition, array, storeIndex + 1, end);
-            quickSort(array, storeIndex + 1, end);
-            //future = executor.submit(quick2);
-            try {
-                future.get();
-                System.out.println("thread ");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } else {
-            quickSort(array, start, storeIndex - 1);
-            quickSort(array, storeIndex + 1, end);
-        }
+        
+		//split array into 2 new threads
+		PSort psort1 = new PSort(array, start, storeIndex - 1);
+		PSort psort2 = new PSort(array, storeIndex + 1, end);
+		Future<?> f1 = executor.submit(psort1);
+		Future<?> f2 = executor.submit(psort2);
+		
+			try{
+				f1.get();
+				f2.get();
+			}
+			catch(InterruptedException  e){
+				e.printStackTrace();
+			}
+			catch(ExecutionException ex){
+				ex.printStackTrace();
+			}
 			
-	} 
+    }
+	
+	public void swap(int[] Array, int x, int y){
+		int temp = Array[x];
+		Array[x] = Array[y];
+		Array[y] = temp;
+	}
+	
+	public static void parallelSort(int[] A, int begin, int end){
+		
+		PSort psort = new PSort(A, begin, end-1);
+		Future<?> future = psort.executor.submit(psort);
+		try{
+			future.get();
+		}
+		catch(InterruptedException  e){
+			e.printStackTrace();
+		}
+		catch(ExecutionException ex){
+			ex.printStackTrace();
+		}
+		psort.executor.shutdown(); // shutdown the thread pool
+	}
+		
 	
 }
