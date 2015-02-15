8b51d834bcdf52c5c8c0778114f1afae7c5e30af
juliana leon <jewelsleon@gmail.com>
2015-02-14 19:17:37 -0600

    initial classes for assignment2

---
+++ 360pAssignment2/src/assignment2/FairReadWriteLock.java
@@ -0,0 +1,21 @@
+package assignment2;
+
+public class FairReadWriteLock {
+	
+	void beginRead(){
+		
+	}
+	
+	void endRead(){
+		
+	}
+	
+	void beginWrite(){
+		
+	}
+	
+	void endWrite(){
+		
+	}
+	
+}
---
+++ 360pAssignment2/src/assignment2/Garden.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class Garden {
+	// Implement the following function
+	// You don’t need to worry about exceptions
+	// The constructor takes the MAX argument
+	public Garden(int MAX){}
+	public void startDigging(){}
+	public void doneDigging(){}
+	public void startSeeding(){}
+	public void doneSeeding(){}
+	public void startFilling(){}
+	public void doneFilling(){}
+}
