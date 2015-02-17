d638e8fe6854ab107a74ddd337a3c6126c0e1d46
juliana leon <jewelsleon@gmail.com>
2015-02-16 18:40:04 -0600

    first attempt at gardening

--- 360pAssignment2/src/assignment2/Garden.java
+++ 360pAssignment2/src/assignment2/Garden.java
@@ -1,14 +1,62 @@
 package assignment2;
 
+import java.util.concurrent.locks.*;
+
 public class Garden {
 	// Implement the following function
 	// You don’t need to worry about exceptions
 	// The constructor takes the MAX argument
-	public Garden(int MAX){}
-	public void startDigging(){}
-	public void doneDigging(){}
-	public void startSeeding(){}
-	public void doneSeeding(){}
-	public void startFilling(){}
-	public void doneFilling(){}
+	final ReentrantLock shovel = new ReentrantLock();
+	//final Condition shovel_taken = lock.newCondition();
+	final Condition can_dig = shovel.newCondition();
+	final Condition can_plant = shovel.newCondition();
+	final Condition can_fill = shovel.newCondition();
+	
+	private int newt = 0;
+	private int ben = 0;
+	private int mary = 0;
+	private int max;
+	
+	public Garden(int MAX){
+		this.max = MAX;
+	}
+	public void startDigging() throws InterruptedException{
+		
+			if(newt-mary == max){
+				can_dig.await();
+			}
+			else{
+				shovel.lock();
+			}	
+	}
+	
+	public void doneDigging(){	
+		newt++;
+		can_plant.signal();
+		shovel.unlock();					//release shovel
+	}
+	
+	public void startSeeding() throws InterruptedException{
+		while(newt <= ben){
+			can_plant.await();
+		}
+	}
+	
+	public void doneSeeding(){
+		ben ++;
+		can_fill.signal();
+	}
+	
+	public void startFilling() throws InterruptedException{
+		while(ben <= mary){
+			can_fill.await();
+		}
+		shovel.lock();
+	}	
+	
+	public void doneFilling(){
+		mary ++;
+		can_dig.signal();						   
+		shovel.unlock();					//release shovel
+	}
 }
