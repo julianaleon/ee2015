2eb422b4a4ebe18e4fd1f49bed759176e6d679db
juliana leon <jewelsleon@gmail.com>
2015-02-28 17:18:15 -0600

    intial commit for assignment 3

---
+++ 360pAssignment3/src/assignment3/PQueue.java
@@ -0,0 +1,35 @@
+package assignment3;
+
+public class PQueue {
+
+	// Creates a Priority queue with maximum allowed size as m
+	public PQueue(int m){
+		
+	}
+	
+	// Inserts the name with its priority in the PQueue.
+	// It returns -1 if the name is already present in the list.
+	// Otherwise, returns the current position in the list where the name was inserted.
+	// This method blocks when the list is full.
+	public int insert(String name, int priority){
+		return -1;
+	}
+	
+	// Returns the position of the name in the list.
+	// If the name is not found it returns -1;
+	public int search(String name){
+		return -1;
+	}
+	
+	// Returns the name with the highest priority in the list.
+	// If the list is empty, then the method blocks.
+	// The name is deleted from the list.
+	public String getFirst(){
+		return "";
+	}
+	
+
+	
+	
+	
+}
