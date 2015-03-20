cf3c8f0c94f4b436af5714119fb4510bc0e89ed7
juliana leon <jewelsleon@gmail.com>
2015-03-20 05:07:13 -0500

    first commit, old server/client code

---
+++ 360pAssignment4/src/assignment4/Client.java
@@ -0,0 +1,131 @@
+package assignment4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+	
+
+public class Client {
+	private InetAddress ipAddress;
+	private String id;
+	
+	public Client(){
+		this.id = "c";
+		this.ipAddress = null;
+	}
+	
+	public void processFirstCommand(String input){
+		String[] setup = input.split("\\s+");
+		this.id = id + setup[0];
+		
+		try {
+			this.ipAddress = InetAddress.getByName(setup[1]);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void processCommands(String input){
+		String[] setup = input.split("\\s+");
+		String book_id = setup[0];
+		String request = setup[1];
+		
+		if(book_id.equals("sleep")){
+			try {
+				Thread.sleep(Integer.parseInt(request));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		else{
+			int port = Integer.parseInt(setup[2]);
+			String protocol = setup[3];
+			
+			if(protocol.equals("T")){
+				connectTCP(book_id, request, port);
+			}
+			else{
+				connectUDP(book_id, request, port);
+			}
+		}
+	}
+	
+	
+	public void connectUDP(String book_id, String request, int port){
+		DatagramSocket socket;
+		DatagramPacket sendPacket;
+		DatagramPacket receivePacket;
+		String command = id + " " + book_id + " " + request;
+		byte[] received_buf = new byte[1024];
+		
+		try {
+			socket = new DatagramSocket();
+			byte[] buf = command.getBytes();
+			
+			sendPacket = new DatagramPacket(buf, buf.length, ipAddress, port);
+			socket.send(sendPacket);
+			
+			receivePacket = new DatagramPacket(received_buf, received_buf.length);
+			socket.receive(receivePacket);
+			
+			String received = new String(receivePacket.getData(), 0, receivePacket.getLength());
+			System.out.println(received);
+			
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void connectTCP(String book_id, String request, int port){
+		Socket socket = null;
+		String command = id + " " + book_id + " " + request;
+			try {
+				socket = new Socket(this.ipAddress, port);
+				Scanner in = new Scanner(socket.getInputStream());
+				PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+				pw.println(command);
+				System.out.println(in.nextLine());
+				
+				socket.close();			//close socket 
+				in.close();				//close scanner
+				pw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public static void main(String argv[]) throws Exception{
+		
+		Client client = new Client();
+		Scanner in = new Scanner(System.in);
+		//String input = in.nextLine();
+		client.processFirstCommand(in.nextLine());
+		
+		while(true){
+		//while(in.hasNextLine()){
+			try{
+				client.processCommands(in.nextLine());
+			}catch(NoSuchElementException e){
+				break;
+			}
+		}
+		
+	}
+	
+}
+
---
+++ 360pAssignment4/src/assignment4/Server.java
@@ -0,0 +1,169 @@
+package assignment4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+	private ServerSocket tSocket;
+	private DatagramSocket uSocket;
+	ExecutorService threadpool;
+	ArrayList<String> book_list;
+	
+	public Server(){
+		book_list = new ArrayList<String>();
+		book_list.add("invalid");
+	}
+	
+	public void processCommand(String input){
+		String[] setup = input.split("\\s+");
+	      
+	    int total_books = Integer.parseInt(setup[0]);
+
+		//System.out.println("total books : " + total_books);
+	      
+	    for(int i=0; i <= total_books; i++){
+	    	book_list.add("Available");
+	    }
+	    
+	    int uPort = Integer.parseInt(setup[1]);
+		int tPort = Integer.parseInt(setup[2]);	
+		
+		try {
+			tSocket = new ServerSocket(tPort);
+			//System.out.println("Listening for TCP on port: " + tPort);
+			uSocket = new DatagramSocket(uPort);
+			//System.out.println("Listening for UDP on port: " + uPort);
+		}catch (SocketException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public class udpListener implements Runnable{
+		int size = 1024;
+		Server myServer; 
+		int port;
+		public udpListener(){}
+		
+		synchronized void sendUDP(){}
+		
+		@Override
+		public void run(){
+			try{
+				while(true){
+					byte[] buf  = new byte[size];
+					DatagramPacket data = new DatagramPacket(buf, buf.length);
+					uSocket.receive(data);
+					
+					String r = new String (data.getData());				//returns <clientid> <bookid> <request>
+					String[] request = r.split("\\s+");
+					String output = Server.this.checkBook(request[0], request[1], request[2]);
+					byte[] output_buf = output.getBytes();	
+					
+					DatagramPacket packet = new DatagramPacket (output_buf, output_buf.length, data.getAddress(), data.getPort());
+					uSocket.send(packet);	
+				}
+			}
+			catch(Exception e){
+				
+			}
+		} 
+	}
+	
+	public class tcpListener implements Runnable{
+		public tcpListener(){};
+		
+		@Override
+		public void run() {
+			Socket socket;
+			try {
+				while ( (socket = tSocket.accept()) != null) {
+					threadpool.submit(new tcpHandler(socket));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}	
+	}
+	
+	public class tcpHandler implements Runnable{
+		Socket socket;
+		
+		public tcpHandler(Socket socket){
+			this.socket = socket;
+		}
+		
+		@Override
+		public void run() {
+			try{
+				Scanner in = new Scanner(socket.getInputStream());
+				PrintWriter pw = new PrintWriter(socket.getOutputStream());
+				String r = in.nextLine();				//returns <clientid> <bookid> <request>
+				String[] request = r.split("\\s+");
+				
+				String output = Server.this.checkBook(request[0], request[1], request[2]);
+				pw.println(output);
+				pw.flush();
+				pw.close();
+			    socket.close();
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public synchronized String checkBook(String client_id , String book_num, String request){
+		int bookNumber = Integer.parseInt(book_num.substring(1));
+		String returnMessage;
+		String status = book_list.get(bookNumber);
+		
+		if (request.equals("reserve")){ 					//check if book is available
+			if (status.equals("Available")){
+				returnMessage = client_id + " " + book_num;
+				book_list.set(bookNumber, client_id);		//checkout book to client
+			}
+			else{											//not available, request fails
+				returnMessage = "fail" + " " + client_id + " " + book_num;
+			}
+		}
+		else{									// return book
+			if(status.equals(client_id)){
+				returnMessage = "free" + " " + client_id + " " + book_num;
+				book_list.set(bookNumber, "Available");			
+			}
+			else{
+				returnMessage = "fail" + " " + client_id + " " + book_num;
+			}
+		}
+		return returnMessage;
+	}
+	
+	
+	public void listener(){
+		threadpool = Executors.newCachedThreadPool();
+		udpListener udpListen = new udpListener();
+		threadpool.submit(udpListen);
+		tcpListener tcpListen = new tcpListener();
+		threadpool.submit(tcpListen);
+		while (true);
+	}
+	
+	public static void main(String argv[]){
+	     
+		Server server = new Server();  
+	    Scanner in = new Scanner(System.in);
+	    String input = in.nextLine();
+
+		//System.out.println("Input to process:" + input);
+	    server.processCommand(input);
+	    server.listener();
+	}	
+		
+}
